89f5eef7396620c739bf856f35eba60269c65b0d
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2017-04-09 18:27:34 +0200

    watki 2

--- src/zajecia2/Watki.java
+++ src/zajecia2/Watki.java
@@ -6,20 +6,20 @@ package zajecia2;
 public class Watki {
 
 
-    public static void main (String[ ] a){
+    public static void main (String[ ] a)
+            throws InterruptedException {
         Zadane z1 = new Zadane("Z1");
         Zadane z2 = new Zadane("Z2");
-        z1.run();
-        z2.run();
+        new Thread(z1).start();
+        new Thread(z2).start();
+        Thread.sleep(10);
+        System.out.println("Koniec programu");
     }
 }
-
 class Zadane implements Runnable{
     String nazwa;
     public Zadane(String nazwa) {
-        this.nazwa = nazwa;
-    }
-    @Override
+        this.nazwa = nazwa;}
     public void run() {
         for (int i=0; i< 50; i++){
             System.out.println(nazwa + " " + i);
